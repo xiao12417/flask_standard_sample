37e54a87c73c65ab3d68a6b0c2e47c4a7594043d
hustcc <i@hust.cc>
2015-08-21 16:07:57 +0800

    add DB

--- app/redis/REDIS.py
+++ app/redis/REDIS.py
@@ -10,19 +10,7 @@ from app.wraps.singleton_wrap import singleton
 
 @singleton
 class REDIS:
-    def __init__(self, host = db_config['DB_HOST'], 
-                        port = db_config['DB_PORT'],
-                        user = db_config['DB_USER'], 
-                        passwd = db_config['DB_PSW'], 
-                        db = db_config['DB_NAME'], 
-                        charset = db_config['DB_CHARSET']):
-        
-        try:
-            self.conn = MySQLdb.connect(host = host, port = port, user = user, passwd = passwd, db = db, charset = charset)
-            #字典形式
-            self.cursor = self.conn.cursor(cursorclass = MySQLdb.cursors.DictCursor)
-            print("Mysql Connect to %s: %s" % (host, str(port)))
-        except MySQLdb.Error as e:
-            print("Mysql Error %s: %s" % (host, e.args[1]))
+    def __init__(self):
+        pass
 
     
